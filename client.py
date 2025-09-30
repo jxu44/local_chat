2d54d62232c2bf4926516f508ced8816cc0c67e6
Jeffrey Xu <jxu743@gatech.edu>
2025-09-29 22:51:04 -0400

    validation

--- client.py
+++ client.py
@@ -4,16 +4,35 @@ import sys
 import argparse
 
 
-# TODO: Implement a client that connects to your server to chat with other clients here
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--user')
-    parser.add_argument('-pass', '--password') 
-    parser.add_argument('-p', "--port")
+    parser.add_argument('-port', "--port")
+    parser.add_argument('-host', "--host")
+    parser.add_argument('-username', '--username')
+    parser.add_argument('-passcode', '--passcode') 
     args = parser.parse_args()
 
-    print(args)
+    if not args.port:
+        print("Please provide a port.")
+        return
+    if not args.host:
+        print("Please provide a host.")
+        return
+    if not args.username or len(args.username) > 8:
+        print("Please provide a valid username.")
+        return
+    if not args.passcode or len(args.passcode) > 5:
+        print("Please provide a valid passcode.")
+        return
+
+
+    host = args.host
+    port =args.port
+    username = args.username
+    password = args.password
+
+    print(host)
 
 
 
--- server.py
+++ server.py
@@ -4,13 +4,24 @@ import sys
 import argparse
 from datetime import datetime, timedelta
 
-#TODO: Implement all code for your server here
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--count') 
-    parser.add_argument('-p', "--port")
+    parser.add_argument('-passcode', '--passcode') 
+    parser.add_argument('-port', "--port")
     args = parser.parse_args()
+
+    if not args.port:
+        print("Please provide a port.")
+        return
+    if not args.passcode or len(args.passcode) > 5:
+        print("Please provide a valid passcode.")
+        return
+
+    passcode = args.passcode
+    port = args.port
+
+    mySocket = socket.socket()
 
     print(args)
 
